2c9ff23873e2a8ddcd66bd7068dfe40175387a5a
lifehackpy <lifehack.py@gmail.com>
2019-11-08 19:27:11 -0500

    refactoring site

--- mentor/urls.py
+++ mentor/urls.py
@@ -3,5 +3,6 @@ from . import views
 # from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+    path('/python-de-cero-a-ninja', views.taller_python, name='mentor'),
     path('', views.mentor, name='mentor'),
 ]
--- mentor/views.py
+++ mentor/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 def mentor(request):
     try:
         mobile = request.user_agent.is_mobile
-    except Exception as e:
+    except:
         mobile = ''
     return render(request, 'base_mentor.html', {'mobile': mobile})
+
+
+def taller_python(request):
+    try:
+        mobile = request.user_agent.is_mobile
+    except:
+        mobile = ''
+    return render(request, 'taller_python.html', {'mobile': mobile})

ef4e8f3797ef28fd28455c864f072e032915bbee
lifehackpy <lifehack.py@gmail.com>
2019-11-14 22:28:11 -0500

    usd

--- startup/views.py
+++ startup/views.py
@@ -106,7 +106,7 @@ def checkout(request):
                 token_payment_gateway,
                 tk_django,
                 email,
-                180*100, # verificar! enviar en centimos
+                175*100, # verificar! enviar en centimos
                 'Taller Python de Cero a Ninja',
                 firstName,
                 lastName,

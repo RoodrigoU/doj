37de6ea865e171a485beae005295257e5b959694
lifehackpy <lifehack.py@gmail.com>
2019-11-16 22:52:06 -0500

    nice

--- mentor/views.py
+++ mentor/views.py
@@ -8,9 +8,9 @@ def mentor(request):
     except:
         pass
     if mobile:
-        uri_whatsapp = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20taller%20Python."
+        uri_whatsapp = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
     else:
-        uri_whatsapp = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20taller%20Python."
+        uri_whatsapp = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
 
     return render(request, 'base_mentor.html', {'uri_whatsapp': uri_whatsapp})
 
@@ -23,9 +23,11 @@ def taller_python(request):
         pass
     if mobile:
         uri_reserva = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%2C%20quisiera%20reservar%20la%20prueba%20gratuita%20del%20Taller%20Python."
-        uri_whatsapp = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20taller%20Python."
+        uri_whatsapp = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
+        uri_buy = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20comprar%20el%20Taller%20de%20Python."
     else:
         uri_reserva = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%2C%20quisiera%20reservar%20la%20prueba%20gratuita%20del%20Taller%20Python."
-        uri_whatsapp = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20taller%20Python."
+        uri_whatsapp = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
+        uri_buy = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20comprar%20el%20Taller%20de%20Python."
 
-    return render(request, 'taller_python.html', {'uri_whatsapp': uri_whatsapp, 'uri_reserva': uri_reserva})
+    return render(request, 'taller_python.html', {'uri_whatsapp': uri_whatsapp, 'uri_reserva': uri_reserva, 'uri_buy':uri_buy})

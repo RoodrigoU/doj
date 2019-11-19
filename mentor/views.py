3e2f2f7b06ecea842d9c555aea08336ecb523185
lifehackpy <lifehack.py@gmail.com>
2019-11-19 15:23:33 -0500

    nice

--- mentor/views.py
+++ mentor/views.py
@@ -38,9 +38,9 @@ def taller_python(request):
         uri_reserva = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%2C%20quisiera%20reservar%20la%20prueba%20gratuita%20del%20Taller%20Python."
         uri_whatsapp = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
         uri_buy = "https://api.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20comprar%20el%20Taller%20de%20Python."
+        return render(request, 'taller_python_mobile.html', {'uri_whatsapp': uri_whatsapp, 'uri_reserva': uri_reserva, 'uri_buy':uri_buy})
     else:
         uri_reserva = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%2C%20quisiera%20reservar%20la%20prueba%20gratuita%20del%20Taller%20Python."
         uri_whatsapp = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20Taller%20Python."
         uri_buy = "https://web.whatsapp.com/send?phone=51935489552&text=Hola%21%20quisiera%20comprar%20el%20Taller%20de%20Python."
-
-    return render(request, 'taller_python.html', {'uri_whatsapp': uri_whatsapp, 'uri_reserva': uri_reserva, 'uri_buy':uri_buy})
+        return render(request, 'taller_python.html', {'uri_whatsapp': uri_whatsapp, 'uri_reserva': uri_reserva, 'uri_buy':uri_buy})

9950aca925fe657d2b03936d6961cdbaf60a3b78
lifehackpy <lifehack.py@gmail.com>
2019-11-19 14:26:02 -0500

    video demo

--- mentor/urls.py
+++ mentor/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('python-de-cero-a-ninja', views.taller_python, name='taller-python'),
-    path('demo', views.demo, name='demo'),
+    path('flujo', views.demo, name='flujo'),
     path('', views.mentor, name='mentor'),
 ]

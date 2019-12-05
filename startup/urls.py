47bd82c6a0304c06c5c21725e8ae8791c073a53a
lifehackpy <lifehack.py@gmail.com>
2019-12-05 18:45:53 -0500

    change uri

--- startup/urls.py
+++ startup/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.decorators.csrf import csrf_exempt
-
+from mentor.views import python_mentor
 urlpatterns = [
     path('', views.home, name='home'),
-    path('python', csrf_exempt(views.python_startup), name='python-startup'),
+    path('python', csrf_exempt(python_mentor), name='python-mentor'),
     path('python-plan-mes-1-2', csrf_exempt(views.python_plan_mes_1_2), name='python-plan'),
     path('checkout', csrf_exempt(views.checkout), name='checkout'),
     path('checkout/success', csrf_exempt(views.checkoutSuccesss), name='checkout-successs'),
